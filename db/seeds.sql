686df8823c8139bd59c231f064ebedd621095592
Keith S Thomas <kthomasnj@hotmail.com>
2022-09-21 10:04:26 -0400

    repaired seeding error on employee's table.

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,7 +19,7 @@ VALUES  ("HR Generalist", 50000, 1),
         ("Machine Operator", 35000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id)
-VALUES  ("Sam", "Harry", 5),
-        ("Terry", "Moore", 6),
-        ("Cedric", "Davis", 7),
-        ("Blane", "McDowell", 8);
+VALUES  ("Sam", "Harry", 1),
+        ("Terry", "Moore", 2),
+        ("Cedric", "Davis", 3),
+        ("Blane", "McDowell", 4);
